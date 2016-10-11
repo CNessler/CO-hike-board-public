4324e3c53fb179aa24dc6785d2197a42deed062e
Claire Nessler <clrnessler@gmail.com>
2016-10-10 20:14:50 -0600

    adding

--- public/angular/factory.js
+++ public/angular/factory.js
@@ -1,24 +1,3 @@
-// app.factory("HikeService", ['$http', function ($http) {
-//   return {
-//     all: function(){
-//       return $http.get('https://co-hike-board.herokuapp.com/api/hikes').then(function (data) {
-//         return data.data;
-//       })
-//     },
-//     create: function (post) {
-//       $http.post('https://co-hike-board.herokuapp.com/api/hikes', post);
-//     },
-//     update: function (comment) {
-//       $http.post('https://co-hike-board.herokuapp.com/api/comment', comment);
-//     },
-//     upvote: function (post) {
-//       $http.post('https://co-hike-board.herokuapp.com/api/upvote', post)
-//     },
-//     downvote: function (post) {
-//       $http.post('https://co-hike-board.herokuapp.com/api/downvote', post)
-//     }
-//   }
-// }])
 app.factory("HikeService", ['$http', function ($http) {
   return {
     all: function(){
@@ -40,3 +19,24 @@ app.factory("HikeService", ['$http', function ($http) {
     }
   }
 }])
+// app.factory("HikeService", ['$http', function ($http) {
+//   return {
+//     all: function(){
+//       return $http.get('http://localhost:3000/api/hikes').then(function (data) {
+//         return data.data;
+//       })
+//     },
+//     create: function (post) {
+//       $http.post('http://localhost:3000/api/hikes', post);
+//     },
+//     update: function (comment) {
+//       $http.post('http://localhost:3000/api/comment', comment);
+//     },
+//     upvote: function (post) {
+//       $http.post('http://localhost:3000/api/upvote', post)
+//     },
+//     downvote: function (post) {
+//       $http.post('http://localhost:3000/api/downvote', post)
+//     }
+//   }
+// }])
