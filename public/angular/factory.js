c5f564c27df76233ea4da24895431653c5660169
Claire Nessler <clrnessler@gmail.com>
2016-10-10 19:01:54 -0600

    change deploy

--- public/angular/factory.js
+++ public/angular/factory.js
@@ -22,21 +22,21 @@
 app.factory("HikeService", ['$http', function ($http) {
   return {
     all: function(){
-      return $http.get('http://localhost:3000/api/hikes').then(function (data) {
+      return $http.get('https://co-hike-board.herokuapp.com/api/hikes').then(function (data) {
         return data.data;
       })
     },
     create: function (post) {
-      $http.post('http://localhost:3000/api/hikes', post);
+      $http.post('https://co-hike-board.herokuapp.com/api/hikes', post);
     },
     update: function (comment) {
-      $http.post('http://localhost:3000/api/comment', comment);
+      $http.post('https://co-hike-board.herokuapp.com/api/comment', comment);
     },
     upvote: function (post) {
-      $http.post('http://localhost:3000/api/upvote', post)
+      $http.post('https://co-hike-board.herokuapp.com/api/upvote', post)
     },
     downvote: function (post) {
-      $http.post('http://localhost:3000/api/downvote', post)
+      $http.post('https://co-hike-board.herokuapp.com/api/downvote', post)
     }
   }
 }])
